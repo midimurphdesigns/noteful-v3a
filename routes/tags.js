c5df9bf9df1d03dfe91763e588749a3d7d058203
midimurphdesigns <midimurphdesigns@gmail.com>
2018-03-23 08:30:20 -0700

    scaffolded out files for tags endpoints

---
+++ routes/tags.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const mongoose = require('mongoose');
+
+const Tag = require('../models/tag');
+
+/* ========== GET/READ ALL ITEMS ========== */
+
+
+
+
+/* ========== GET/READ A SINGLE ITEM ========== */
+
+
+
+/* ========== POST/CREATE AN ITEM ========== */
+
+
+
+/* ========== PUT/UPDATE A SINGLE ITEM ========== */
+
+
+
+/* ========== DELETE/REMOVE A SINGLE ITEM ========== */
+
+module.exports = router;
